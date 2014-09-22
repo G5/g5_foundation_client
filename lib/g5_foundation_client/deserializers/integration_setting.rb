862d19c7ca6c04c24b6c019698d27b103928f9e3
Perry Hertler <perry@hertler.org>
2014-09-22 17:31:31 -0500

    adding lead integration settings

--- lib/g5_foundation_client/deserializers/integration_setting.rb
+++ lib/g5_foundation_client/deserializers/integration_setting.rb
@@ -9,12 +9,17 @@ module G5FoundationClient::Deserializers
 
       def hcard_to_hash(h)
         {
-            inventory_service_url: with_safe_access { h.g5_inventory_service_url },
-            etl_strategy_name:     with_safe_access { h.g5_etl_strategy_name },
-            vendor_endpoint:       with_safe_access { h.g5_vendor_endpoint },
-            vendor_user_name:      with_safe_access { h.g5_vendor_user_name },
-            vendor_password:       with_safe_access { h.g5_vendor_password },
-            custom_integration_settings: custom_integration_settings_from_hcard(h)
+            inventory_service_url:        with_safe_access { h.g5_inventory_service_url },
+            inventory_service_auth_token: with_safe_access { h.g5_inventory_service_auth_token },
+            etl_strategy_name:            with_safe_access { h.g5_etl_strategy_name },
+            inventory_vendor_endpoint:    with_safe_access { h.g5_inventory_vendor_endpoint },
+            inventory_vendor_user_name:   with_safe_access { h.g5_inventory_vendor_user_name },
+            inventory_vendor_password:    with_safe_access { h.g5_inventory_vendor_password },
+            lead_strategy_name:           with_safe_access { h.g5_lead_strategy_name },
+            lead_vendor_endpoint:         with_safe_access { h.g5_lead_vendor_endpoint },
+            lead_vendor_user_name:        with_safe_access { h.g5_lead_vendor_user_name },
+            lead_vendor_password:         with_safe_access { h.g5_lead_vendor_password },
+            custom_integration_settings:  custom_integration_settings_from_hcard(h)
         }
       end
 
--- lib/g5_foundation_client/models/integration_setting.rb
+++ lib/g5_foundation_client/models/integration_setting.rb
@@ -6,10 +6,15 @@ class G5FoundationClient::IntegrationSetting
   end
 
   attribute :inventory_service_url, String
+  attribute :inventory_service_auth_token, String
   attribute :etl_strategy_name, String
-  attribute :vendor_endpoint, String
-  attribute :vendor_user_name, String
-  attribute :vendor_password, String
+  attribute :inventory_vendor_endpoint, String
+  attribute :inventory_vendor_user_name, String
+  attribute :inventory_vendor_password, String
+  attribute :lead_strategy_name, String
+  attribute :lead_vendor_endpoint, String
+  attribute :lead_vendor_user_name, String
+  attribute :lead_vendor_password, String
   attribute :custom_integration_settings, Hash
 
   def to_settings_hash
